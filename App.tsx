725eda9b570965ad6acc77cd0fbd8dc49d7363b0
Yariv Gilad <yariv.gilad@gmail.com>
2022-12-09 19:54:43 +0200

    indexed all screens

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import React, {FC} from 'react';
-import LoginScreen from './src/screens/public/login/login.screen';
-// import RegisterScreen from './src/screens/public/register/register.screen';
-// import ForgotPasswordScreen from './src/screens/public/forgot-password/forgot-password.screen';
-// import HomeScreen from './src/screens/protected/home/home.screen';
-// import NotificationsScreen from './src/screens/protected/notifications/notifications.screen';
-// import WalletScreen from './src/screens/protected/wallet/wallet.screen';
-// import SettingsScreen from './src/screens/protected/settings/settings.screen';
-// import SettingsDetailScreen from './src/screens/protected/setting-details/settings-detail.screen';
+import {
+  LoginScreen,
+  RegisterScreen,
+  ForgotPasswordScreen,
+  HomeScreen,
+  NotificationsScreen,
+  WalletScreen,
+  SettingsScreen,
+  SettingsDetailScreen,
+} from './src/screens';
+
 
 const App: FC = () => {
   return <LoginScreen />;
